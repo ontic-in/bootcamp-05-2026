c16a72d024c4cb93ab78cf6f6a1ffc5aaecb9f3d
Vaidehi Joshi <vaidehijoshi@Vaidehis-MacBook-Pro.local>
2026-05-05 11:19:38 +0530

    red: diagonal line

--- tests/test.js
+++ tests/test.js
@@ -20,3 +20,10 @@ describe('lengthOfVerticalLine', () => {
       expect(distance).toBe(5);
     });
   });
+
+describe('lengthOfDiagonalLine', () => {
+    it('the length of line between (0,0) and (5,5) should be 5', () => {
+      const distance = distanceBetweenPoints(0,0,5,5);
+      expect(distance).toBe(5);
+    });
+  });
