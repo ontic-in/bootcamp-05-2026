e5f2ac7377110bb4daba9f865c1f1146db0c5f15
Vaidehi Joshi <vaidehijoshi@Vaidehis-MacBook-Pro.local>
2026-05-05 11:05:56 +0530

    red: horizontal line

--- tests/test.js
+++ tests/test.js
@@ -6,3 +6,10 @@ describe('lengthOfLine', () => {
       expect(distance).toBe(0);
     });
   });
+
+describe('lengthOfHorizontalLine', () => {
+    it('the length of line between (0,0) and (5,0) should be 0', () => {
+      const distance = distanceBetweenPoints(0,0,5,0);
+      expect(distance).toBe(5);
+    });
+  });
