52e2c12d53fab282479860e3972805c46a908a90
Vaidehi Joshi <vaidehijoshi@Vaidehis-MacBook-Pro.local>
2026-05-05 11:13:25 +0530

    red: vertical line

--- tests/test.js
+++ tests/test.js
@@ -8,8 +8,15 @@ describe('lengthOfLine', () => {
   });
 
 describe('lengthOfHorizontalLine', () => {
-    it('the length of line between (0,0) and (5,0) should be 0', () => {
+    it('the length of line between (0,0) and (5,0) should be 5', () => {
       const distance = distanceBetweenPoints(0,0,5,0);
+      expect(distance).toBe(5);
+    });
+  });
+
+describe('lengthOfVerticalLine', () => {
+    it('the length of line between (0,0) and (0,5) should be 5', () => {
+      const distance = distanceBetweenPoints(0,0,0,5);
       expect(distance).toBe(5);
     });
   });
