79f9f2e7ea089cf5cb0bf713a5c523195c5173c0
Vaidehi Joshi <vaidehijoshi@Vaidehis-MacBook-Pro.local>
2026-05-05 14:41:08 +0530

    red: point as a line

--- geometry-line/index.js
+++ geometry-line/index.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
 
 function distanceBetweenPoints(x1, y1, x2, y2){
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    return;
 }
 
 if (require.main === module) {
--- geometry-line/tests/test.js
+++ geometry-line/tests/test.js
@@ -1,29 +1,8 @@
 const { distanceBetweenPoints } = require('../index');
 
 describe('lengthOfLine', () => {
-    it('the length of line between (0,0) and (0,0) should be 0', () => {
+    it('the length of the line starting and ending at same point should be 0', () => {
       const distance = distanceBetweenPoints(0,0,0,0);
       expect(distance).toBe(0);
-    });
-  });
-
-describe('lengthOfHorizontalLine', () => {
-    it('the length of line between (0,0) and (5,0) should be 5', () => {
-      const distance = distanceBetweenPoints(0,0,5,0);
-      expect(distance).toBe(5);
-    });
-  });
-
-describe('lengthOfVerticalLine', () => {
-    it('the length of line between (0,0) and (0,5) should be 5', () => {
-      const distance = distanceBetweenPoints(0,0,0,5);
-      expect(distance).toBe(5);
-    });
-  });
-
-describe('lengthOfDiagonalLine', () => {
-    it('the length of line between (0,0) and (5,5) should be 5', () => {
-      const distance = distanceBetweenPoints(0,0,5,5);
-      expect(distance).toBe(7.0710678118654755);
     });
   });
